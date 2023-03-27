8d19811d8501b13cbbeab3430b1b50fab1118a2a
NoemiCouch <Noemicouch@gmail.com>
2023-03-27 12:59:59 -0500

    newstuff

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+const currentDateElement = $('#currentDay')
 $(function () {
+  const currentDate = dayjs().format('ddd MMMM DD, YYYY')
+  console.log(currentDate)
+  // currentDateElement.textContent Raw JS version
+  //vvv Jquery Version vvv
+   currentDateElement.text(currentDate)
+
+   const currentHour = dayjs().format('H')
+   console.log(currentHour)
+
+   /*
+   A conditional statement that checks every timeblock
+   */
+
+  // Mon March 27, 2023
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
